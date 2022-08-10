7935f12575fac6c40664ae093f8ff4f257ec4adc
Nathan Furnal <nathan_furnal@hotmail.com>
2022-08-10 18:52:28 +0200

    update keys generation provide user deletion with all related files

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -10,7 +10,9 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Validation\Rules;
-use phpseclib3\Crypt\RSA;
+use ParagonIE\Halite\KeyFactory;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Storage;
 
 class RegisteredUserController extends Controller
 {
@@ -39,19 +41,24 @@ class RegisteredUserController extends Controller
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
         ]);
-        $private = RSA::createKey();
-        $public = $private->getPublicKey();
+        $enc_keyPair = KeyFactory::generateEncryptionKeyPair();
+        $sign_keyPair = KeyFactory::generateEncryptionKeyPair();
+        $private_enc = $enc_keyPair->getSecretKey();
+        $private_sign = $sign_keyPair->getSecretKey();
+        $public_enc = $enc_keyPair->getPublicKey();
+        $public_sign = $sign_keyPair->getPublicKey();
         $path = base_path() . '/privateKeys/' . $request->email . '/';
         if(!is_dir($path))
             mkdir($path, 0777, true);
-        file_put_contents($path . 'key.pem', $private);
-        
+        KeyFactory::save($private_enc, $path.'key.pem');
+        KeyFactory::save($private_sign, $path.'sign.pem');
         $user = User::create([
             'name' => $request->name,
             'email' => $request->email,
             'password' => Hash::make($request->password),
             'validated' => false, // default validated value
-            'public_key_enc' => $public,
+            'public_key_enc' => KeyFactory::export($public_enc)->getString(),
+            'public_key_sign' => KeyFactory::export($public_sign)->getString(),
         ]);
 
         event(new Registered($user));
@@ -59,5 +66,16 @@ class RegisteredUserController extends Controller
         //        Auth::login($user); Do NOT login automatically once the user is created
         Auth::logout();
         return redirect(RouteServiceProvider::HOME);
+    }
+
+    public function destroy(){
+        $id = Auth::user()->id;
+        $email = DB::table('users')->where('id', $id)->get()->first()->email;
+        $files = DB::table('files')->where('owner_id', $id)->get('name')->toArray();
+        $files = array_map(function($item){return $item->name;}, $files);
+        Storage::disk('local')->delete($files);
+        Storage::disk('keys')->deleteDirectory($email);
+        DB::table('users')->where('id', $id)->delete();
+        return redirect('register');
     }
 }
